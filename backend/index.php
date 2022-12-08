946a67bee9507c21c2740beabed8dbbec9ecaef1
hermanmans <hsmans0601@gmail.com>
2022-12-08 13:06:21 +0200

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -6,10 +6,10 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require './vendor/autoload.php';// This is compulsory for our framework to work, but don't worry too much about how it works
 
-/*$app = new \Slim\App;// We create an object of the Slim framework main app
+$app = new \Slim\App;// We create an object of the Slim framework main app
 
 var_dump ($app); ///////////////////TEST tos see if slim works
-*/
+
 
 
 
