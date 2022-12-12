8e74f9f4e97d1ade5c9249a27c1270e8cdeb1b82
hermanmans <hsmans0601@gmail.com>
2022-12-12 14:39:27 +0200

    updating configuration files

---
+++ Backend/PHP/functions.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+function pdo_connect_mysql() {
+    // Update the details below with your MySQL details
+    //$DATABASE_HOST = "sql7.freesqldatabase.com";
+    //$DATABASE_USER = "sql7583456";
+    //$DATABASE_PASS = "gjXWE9H8YW";
+    //$DATABASE_NAME = "sql7583456";
+    $servername = "sql7.freesqldatabase.com";
+    $username = "sql7583456";
+    $password = "gjXWE9H8YW";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=sql7583456", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    phpinfo();
+    mysql_ping();
+    }
+
+}
+function template_header($title) {
+echo <<<EOT
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link rel="stylesheet" src="/CSS/stylesheet.css">
+        <title>$title</title>
+    </head>
+    <body>
+        <header>
+            <div class="content-wrapper">
+                <h1>Shopping Cart System</h1>
+                <nav>
+                    <a href="index.php">Home</a>
+                    <a href="index.php?page=products">Products</a>
+                </nav>
+                <div class="link-icons">
+                    <a href="index.php?page=cart">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </div>
+            </div>
+        </header>
+        <main>
+EOT;
+};
+    // Template footer
+    function template_footer() {
+    $year = date('Y');
+    echo <<<EOT
+            </main>
+            <footer>
+                <div class="content-wrapper">
+                    <p>&copy; $year, Shopping Cart System</p>
+                </div>
+            </footer>
+        </body>
+</html>
+EOT;
+}
+?>
